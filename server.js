e242d2e71fe343cb690720edc345b4ec80d725f0
dxc2007 <duxiaochen1996@outlook.com>
2016-11-21 16:57:46 +0800

    port issue fixed

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
+const port = process.env.PORT || 8080;
+
 app.use(express.static("./public"));
 
-app.listen(8080, function() {
-  console.log("Pirates on Deck 8080" );
+app.listen(port, function() {
+  console.log("Pirates on Deck", port);
 });
