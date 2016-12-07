1d2f42acb1c09f8fc46596631f467f61fe7a0b78
dxc2007 <duxiaochen1996@outlook.com>
2016-12-07 23:16:29 +0800

    updated some stuff

--- client/components/Navigation.jsx
+++ client/components/Navigation.jsx
@@ -1,23 +1,1 @@
-import React from 'react';
-import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
-
-export default class Navigation extends React.Component {
-  render() {
-    return (
-      <Navbar inverse staticTop>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="/">Goal Based Foodie</a>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Nav pullRight>
-              <NavItem eventKey={1} href="#"></NavItem>
-              <NavItem eventKey={2} href="#"></NavItem>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-    )
-  }
-}
+﻿import React from 'react';import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';export default class Navigation extends React.Component {  render() {    return (      <Navbar inverse staticTop>          <Navbar.Header>            <Navbar.Brand>              <a href="/">Goal Based Foodie</a>            </Navbar.Brand>            <Navbar.Toggle />          </Navbar.Header>          <Navbar.Collapse>            <Nav pullRight>              <NavItem eventKey={1} href="#">Nothing</NavItem>              <NavItem eventKey={2} href="#">Yet</NavItem>            </Nav>          </Navbar.Collapse>        </Navbar>    )  }}
