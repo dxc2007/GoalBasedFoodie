74682489145124b66cb30a8ff71f7ca14ba63e9b
Xiaochen Du <dxc2007@users.noreply.github.com>
2017-01-01 09:50:10 -0500

    Update Homepage.jsx

--- client/components/Homepage.jsx
+++ client/components/Homepage.jsx
@@ -48,7 +48,7 @@ export default class Homepage extends React.Component {
                     <Button bsStyle="primary" bsSize="large" onClick={this.geolocate.bind(this)}><Glyphicon glyph="map-marker" /></Button>
                     {'   '}
                     <Button type="submit" bsSize="large">Submit</Button>
-                    <FoursquareAutoComplete/>
+                    {/*<FoursquareAutoComplete/>*/}
                     {/*<ExportToGoogleCal/>*/}
                 </Form>
             </Col>
